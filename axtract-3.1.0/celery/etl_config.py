cefdec9c2358fcb5e8d1fe8328d594ca4b6bc6a9
Paulo Francesco Pacheco <pfpacheco@gmail.com>
2020-02-15 02:54:15 -0300

    adjustings for ips and celery requirements

--- axtract-3.1.0/celery/etl_config.py
+++ axtract-3.1.0/celery/etl_config.py
@@ -1,12 +1,12 @@
 # The node were rabbitmq is running.
-BROKER_URL = 'amqp://guest:guest@127.0.0.1:5672//'
+BROKER_URL = 'amqp://guest:guest@172.17.0.2:5672//'
 
 # With this setting celeryd kills the worker process for each task.
 # Useful if your tasks use much memory.
-CELERYD_MAX_TASKS_PER_CHILD = 1
+CELERYD_MAX_TASKS_PER_CHILD = 2
 
 # Specify how many processes are used for celeryd tasks.
-CELERYD_CONCURRENCY = 1
+CELERYD_CONCURRENCY = 4
 
 # The default logging format is not nice.
 CELERYD_LOG_FORMAT = "[%(asctime)s: %(levelname)s/%(processName)s] %(name)s - %(message)s"
