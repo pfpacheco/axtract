7b9cfe7d03c39ca96ed24d7993b80fcc62e78a8b
Paulo Francesco Pacheco <pfpacheco@gmail.com>
2020-03-06 09:28:09 -0300

    Loading the axtract 3.2.1 container and adjusting the old 3.1.0 composing

---
+++ axtract-3.2.1/celery/etl_config.py
@@ -0,0 +1,12 @@
+# The node were rabbitmq is running.
+BROKER_URL = 'amqp://guest:guest@rabbitmq:5672//'
+
+# With this setting celeryd kills the worker process for each task.
+# Useful if your tasks use much memory.
+CELERYD_MAX_TASKS_PER_CHILD = 2
+
+# Specify how many processes are used for celeryd tasks.
+CELERYD_CONCURRENCY = 4
+
+# The default logging format is not nice.
+CELERYD_LOG_FORMAT = "[%(asctime)s: %(levelname)s/%(processName)s] %(name)s - %(message)s"
---
+++ axtract-3.2.1/custom_code/axtract.py
@@ -0,0 +1,6 @@
+#!/opt/axtract/bin/call_with_eggs
+
+
+def hello():
+    """ This is the hello from axtract """
+    print "This is the axtract hello !!!"
