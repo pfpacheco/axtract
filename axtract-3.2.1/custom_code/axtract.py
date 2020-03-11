8bae3ddf2e67cc1098fb1ec448b8b32b1a19dae0
Paulo Francesco Pacheco <pfpacheco@gmail.com>
2020-03-11 12:40:28 -0300

    implementing custom event source for axtract deployment validation test

--- axtract-3.2.1/custom_code/axtract.py
+++ axtract-3.2.1/custom_code/axtract.py
@@ -1,6 +1,47 @@
 #!/opt/axtract/bin/call_with_eggs
 
+from ax.eventing.events import timer_event_factory
+import logging
+import time
 
-def hello():
-    """ This is the hello from axtract """
-    print "This is the axtract hello !!! How are you ???"
+
+class HelloFromAxtract(object):
+
+    def __init__(self, caller, name, config, index, chain=None):
+        """ This is the init method """
+        # When you use this skeleton, move these imports to the top of the file.
+
+        self.logger = logging.getLogger("%s.%s" % (__name__, name))
+        self.logger.debug("HelloFromAxtract.__init__: starting...")
+
+        # convert the config objects into a python dictionary
+        config = {item['name_']: item['value'] for item in config.values()}
+
+        self.caller = caller
+        self.name = name
+        self.config = config
+        self.index = index
+        self.ev_mod_chain = chain
+        self.logger.debug("HelloFromAxtract.__init__: finished.")
+
+    def run(self):
+        self.logger.debug("HelloFromAxtract.run: starting...")
+
+        while not self.caller.shutdown_requested:
+            try:
+                time.sleep(1)
+                event = timer_event_factory(self.name)
+                self.ev_mod_chain.run(event)
+            except Exception as ex:
+                self.logger.exception("Error while doing eternal loop: %s", str(ex))
+            else:
+                self.logger.debug("Event successfully created and processed")
+
+            self.logger.debug("Message: %s", self.config.get("the_hello"))
+
+        self.logger.debug("HelloFromAxtract.run: finished.")
+
+
+def hello_from_axtract_factory(caller, name, config, index, chain=None):
+    """ This is the standard factory name for hello """
+    return HelloFromAxtract(caller, name, config, index, chain=chain)
